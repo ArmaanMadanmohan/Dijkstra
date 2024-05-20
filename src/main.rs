a56b21fdd91debe9a48023a1ade778d02cdcd97a
Armaan Madanmohan <armaanmadanmohan@gmail.com>
2024-05-21 03:34:55 +0530

    Implemented minheap functionality

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,87 @@
 
-// let mut vertice: i32;
-// implement graph struct
+// // let mut vertice: i32;
+// // implement graph struct
+// #[derive(Clone)]
+// struct Node {
+//     id: i32,
+//     edges: Vec<Edge>,
+// }
 
-struct Node {
-    id: i32,
-    edges: Vec<Edge>,
-}
+// #[derive(Clone)]
+// struct Edge {
+//     weight: i32,
+//     node1: Node,
+//     node2: Node,
+// }
 
-struct Edge {
-    weight: i32,
-    node1: Node,
-    node2: Node,
-}
 
+// fn main() {
+//     // println!("Hello, world!");
+// }
+
+// // fn make_graph(size: i32) -> Vec<Node> {
+// //     let mut graph: Vec<Node> = Vec::new();
+// //     for i in 0..size {
+// //         let node = Node {
+// //             id: i,
+// //             edges: Vec::new(),
+// //         };
+// //         graph.push(node);
+// //     }
+// //     graph
+// // }
+
+// // fn minDistance(dist: &[i32], spSet: &[bool]) -> usize {
+    
+// // }
+
+// // fn shortest_paths(graph: Vec<Node>, start: Node) -> Vec<i32> {
+// //     // let mut visited: Vec<Node> = Vec::new();
+// //     let mut distance: Vec<i32> = Vec::new();
+// //     let mut unvisited: Vec<Node> = graph;
+// //     for node in &unvisited {
+// //         distance.push(i32::MAX);
+// //     }
+// //     distance[start.id as usize] = 0;
+// //     while !unvisited.is_empty() {
+// //         let mut min = i32::MAX;
+// //         let mut min_index = 0;
+// //         for i in 0..unvisited.len() {
+// //             if distance[unvisited[i].id as usize] < min {
+// //                 min = distance[unvisited[i].id as usize];
+// //                 min_index = i;
+// //             }
+// //         }
+// //         let n = unvisited.remove(min_index); 
+// //         for edge in n.edges {
+// //             let m = opposite(n, &edge);
+// //             let d = distance[n.id as usize] + edge.weight;
+// //             if d < distance[edge.node2.id as usize] {
+// //                 distance[edge.node2.id as usize] = d;
+// //             }
+// //         } 
+// //     }
+// //     distance
+// // }
+
+// // fn opposite(node: &Node, edge: &Edge) -> Node {
+// //      if node.id == edge.node1.id {
+// //          edge.node2.clone()
+// //      } else if node.id == edge.node2.id{
+// //          edge.node1.clone()
+// //      } else {
+// //          panic!("Node is not in the edge");
+// //      }
+// // }
+
+// // provide 
+
+// fn shortest_paths(graph: Vec<Node>, start: Node) {
+//     let mut visited: Vec<Node> = Vec::new();
+//     let mut distance: Vec<i32> = Vec::new();
+
+// }
 
 fn main() {
-    // println!("Hello, world!");
-}
-
-// fn minDistance(dist: &[i32], spSet: &[bool]) -> usize {
     
-// }
-
-fn shortest_paths(graph: Vec<Node>, start: Node) {
-    let mut visited:Vec<Node> = Vec::new();
-    let mut distance:Vec<i32> = Vec::new();
-    let mut unvisited = graph;
-    for node in unvisited {
-        distance.push(i32::MAX);
-    }
-    distance[start.id as usize] = 0;
-    while unvisited.len() > 0 {
-        let mut min = i32::MAX;
-        let mut min_index = 0;
-        for i in 0..unvisited.len() {
-            if distance[i] < min {
-                min = distance[i];
-                min_index = distance[i];
-            }
-        }
-        
-    }
 }
-
-// fn create_link(node1: Node, node2: Node, weight: i32) -> Edge {
-//     Edge {
-//         weight: weight,
-//         node1: node1,
-//         node2: node2,
-//     }
-// }
-
--- src/minheap.rs
+++ src/minheap.rs
@@ -1,14 +1,17 @@
-use std::mem;
 
+// use std::cmp::Ord;
+
+#[derive(Debug)]
 pub struct MinHeap<T: Ord> {
     heap: Vec<T>,
 }
 
-impl <T> MinHeap<T: Ord> {
+impl<T:Ord + Clone> MinHeap<T> {
 
     //Initialises a minimum heap
     fn init() -> MinHeap<T> {
         MinHeap {
+            // heap: Vec::<T>::new(),
             heap: Vec::new(),
         }
     }
@@ -16,7 +19,7 @@ impl <T> MinHeap<T: Ord> {
     //Inserts a value into the heap and maintans heap property
     fn insert(&mut self, value: T) {
         self.heap.push(value);
-        self.heap.heapify_up();
+        self.heapify_up();
     }
 
     //Starts from the last node and swaps with its parent if it is smaller, continuing upwards
@@ -25,7 +28,7 @@ impl <T> MinHeap<T: Ord> {
         while i > 0 {
             let parent = (i - 1) /2;
             if self.heap[i] < self.heap[parent] {
-                self.heap.swap(&mut i, &mut parent);
+                self.heap.swap(i, parent);
                 i = parent;
             }
             else {
@@ -48,7 +51,7 @@ impl <T> MinHeap<T: Ord> {
                 smallest = right_child;
             }
             if smallest != i {
-                self.heap.swap(&mut i, &mut smallest);
+                self.heap.swap(i, smallest);
                 i = smallest;
             }
             else {
@@ -57,16 +60,33 @@ impl <T> MinHeap<T: Ord> {
         }
     }
 
-    fn remove_min(&mut self, index: usize) -> Option<T> {
+    fn remove_min(&mut self) -> Option<T> {
+        println!("{:?}",self.heap.len());
         if self.heap.len() == 0 {
             None
         }
         else {
-            let min = self.heap[0];
-            self.heap[0] = self.heap.pop();
-            let mut i = 0;
-            while (self.heap[i] * )
+            let last_idx = self.heap.len() - 1;
+            self.heap.swap(0, last_idx);
+            let min = self.heap.pop()?;
+            if !self.heap.is_empty() {
+                self.heapify_down();
+            }
+            Some(min)
         }  
     } 
 
+}
+
+fn main() {
+    let mut heap = MinHeap::init();
+    heap.insert(3);
+    heap.insert(1);
+    heap.insert(4);
+    heap.insert(5);
+
+    println!("{:?}", heap.remove_min());
+    println!("{:?}", heap.remove_min());
+    println!("{:?}", heap.remove_min());
+    println!("{:?}", heap.remove_min());
 }
